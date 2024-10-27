7027fa22864159d8723991c0dd6424b075c81743
camalzadeelvin <camalzadeelvin@STHQ0126-01>
2024-10-27 13:19:34 +0400

    new properties

--- GitApp-5/Dtos/ProductDto.cs
+++ GitApp-5/Dtos/ProductDto.cs
@@ -2,7 +2,8 @@
 {
     public class ProductDto
     {
-        public string Name { get; set; }
         public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
     }
 }
